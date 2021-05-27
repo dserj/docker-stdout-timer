faf5a7c5b250fa08c7f7bf8a04b0a44d22594181
Sergei Denikin <sergei_denikin@epam.com>
2021-05-27 11:04:04 +0300

    JSON stringify added

--- timer.js
+++ timer.js
@@ -4,7 +4,7 @@ console.log(`Timeout: ${timeout}`);
 
 function printTime() {
   console.log(new Date(Date.now()).toISOString());
-  console.log({ "jsonDate": new Date(Date.now()).toISOString() });
+  console.log(JSON.stringify({ "jsonDate": new Date(Date.now()).toISOString() }));
   setTimeout(printTime, timeout);
 }
 
